42634f3582598cde17b3c64f6c73eea24487ad39
CharlieChanMeyer <charlie.chan.meyer@gmail.com>
2022-12-15 16:12:05 +0900

    added empty loginActivity

--- app/src/main/java/com/thesis/asecp/Menu.kt
+++ app/src/main/java/com/thesis/asecp/Menu.kt
@@ -3,9 +3,9 @@ package com.thesis.asecp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Menu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.menu)
     }
 }

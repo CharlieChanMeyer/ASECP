113295f9bf367fb5b27a806a79e1f2636463108e
antoinesouchaud <antoine.souchaud@ensea.fr>
2022-12-15 16:19:49 +0900

    Added all empty activity and .xml files

---
+++ app/src/main/java/com/thesis/asecp/TakePicture.kt
@@ -0,0 +1,11 @@
+package com.thesis.asecp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TakePicture : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_take_picture)
+    }
+}

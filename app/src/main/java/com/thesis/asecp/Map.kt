5fe2986c1ca94217d18ae6de6bc10f6bdf8ae0c5
CharlieChanMeyer <charlie.chan.meyer@gmail.com>
2023-01-04 14:35:24 +0900

    First test with mapTiler. Show a map of Nakamozu

--- app/src/main/java/com/thesis/asecp/Map.kt
+++ app/src/main/java/com/thesis/asecp/Map.kt
@@ -1,11 +1,101 @@
 package com.thesis.asecp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.camera.CameraPosition
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.maps.MapView
 
 class Map : AppCompatActivity() {
+
+    private var mapView: MapView? = null
+    lateinit var menuButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val mapTilerKey = getMapTilerKey()
+        validateKey(mapTilerKey)
+        val styleUrl = "https://api.maptiler.com/maps/streets-v2/style.json?key=${mapTilerKey}";
+
+        // Get the MapBox context
+        Mapbox.getInstance(this, null)
+
+        // Set the map view layout
         setContentView(R.layout.activity_map)
+
+        //Create menu button
+        menuButton = findViewById(R.id.menuMapButton)
+        menuButton.setOnClickListener {
+            var intent = Intent(this,Menu::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        // Create map view
+        mapView = findViewById(R.id.mapView)
+        mapView?.onCreate(savedInstanceState)
+        mapView?.getMapAsync { map ->
+            // Set the style after mapView was loaded
+            map.setStyle(styleUrl) {
+                map.uiSettings.setAttributionMargins(15, 0, 0, 15)
+                // Set the map view center
+                map.cameraPosition = CameraPosition.Builder()
+                    .target(LatLng(34.546289,135.503006 ))
+                    .zoom(10.0)
+                    .build()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.onDestroy()
+    }
+
+    private fun getMapTilerKey(): String? {
+        return "PdsAIxHp0n2Oc1Yb2BFo"
+    }
+
+    private fun validateKey(mapTilerKey: String?) {
+        if (mapTilerKey == null) {
+            throw Exception("Failed to read MapTiler key from info.plist")
+        }
+        if (mapTilerKey.lowercase() == "placeholder") {
+            throw Exception("Please enter correct MapTiler key in module-level gradle.build file in defaultConfig section")
+        }
     }
 }
--- app/src/main/java/com/thesis/asecp/Menu.kt
+++ app/src/main/java/com/thesis/asecp/Menu.kt
@@ -1,13 +1,26 @@
 package com.thesis.asecp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.thesis.asecp.R
 
 class Menu : AppCompatActivity() {
+
+    lateinit var buttonMap: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.menu)
+
+        buttonMap = findViewById(R.id.mapbutton)
+        buttonMap.setOnClickListener {
+            var intent = Intent(this, com.thesis.asecp.Map::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
 

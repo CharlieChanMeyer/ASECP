b4656342249e9a97af31b58ffeda51cb84086af4
CharlieChanMeyer <charlie.chan.meyer@gmail.com>
2023-01-04 16:31:16 +0900

    First test google map. Files okay but fragment make app crash

--- app/src/main/java/com/thesis/asecp/Menu.kt
+++ app/src/main/java/com/thesis/asecp/Menu.kt
@@ -1,13 +1,26 @@
 package com.thesis.asecp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.thesis.asecp.R
 
 class Menu : AppCompatActivity() {
+
+    lateinit var mapButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.menu)
+
+        mapButton = findViewById(R.id.mapButtion)
+        mapButton.setOnClickListener {
+            var intent = Intent(this,com.thesis.asecp.Map::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
 

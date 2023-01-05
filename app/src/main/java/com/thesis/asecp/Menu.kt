e7e42ec00f3ebc4d4834770f6ca3522a10aad828
CharlieChanMeyer <charlie.chan.meyer@gmail.com>
2023-01-05 18:23:50 +0900

    MapBox Map+Location codded

--- app/src/main/java/com/thesis/asecp/Menu.kt
+++ app/src/main/java/com/thesis/asecp/Menu.kt
@@ -1,13 +1,25 @@
 package com.thesis.asecp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.thesis.asecp.R
 
 class Menu : AppCompatActivity() {
+
+    lateinit var mapButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.menu)
+
+        mapButton = findViewById(R.id.mapButton)
+        mapButton.setOnClickListener {
+            var intent = Intent(this,com.thesis.asecp.Map::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
 
